dfa6728a1443108c664ae0d38c8a008f17d598f1
Arina <avernidub@ourgapps.com>
2020-06-17 21:47:49 +0300

    add offer request

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 var map = document.querySelector('.map');
-var adsMap = window.generateOfferMap();
 var form = document.querySelector('.ad-form');
 var fieldsets = form.querySelectorAll('fieldset');
 var mainPin = document.querySelector('.map__pin--main');
@@ -17,13 +16,12 @@ var unlockPage = function (evt) {
   mainPin.removeEventListener('keydown', unlockPage);
 };
 
-map.append(window.createPins(adsMap));
-
-window.addPinClickHandler(map, adsMap);
-
+window.load('https://javascript.pages.academy/keksobooking/data', function (data) {
+  map.append(window.createPins(data));
+  window.addPinClickHandler(map, data);
+});
 window.lockFieldsets(fieldsets);
 window.setAddress(form.querySelector('#address'), mainPin);
 window.validateForm(form);
-
 mainPin.addEventListener('mousedown', unlockPage);
 mainPin.addEventListener('keydown', unlockPage);
